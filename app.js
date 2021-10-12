44d41deaf72a564b2abe9d496adac974b0ef34cc
ClarkUPS <90676910+ClarkUPS@users.noreply.github.com>
2021-10-11 22:52:05 -0700

    added music buttons and functionality. Just began work on the clark feild interface to make it function with the add and subtract buttons and drop down menus

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 //Feilds for storing data
-away = new Array() //to store values 
-home = new Array()
-awayTotal = 0
-homeTotal = 0
-
+away = new Array(); //to store values
+home = new Array();
+awayTotal = 0;
+homeTotal = 0;
 
 //Functions:
 
 function addTo() {
-    alert(home_or_away)
-
+  alert(home_or_away);
 }
 
-function subFrom() {
-    
-
-}
+function subFrom() {}
 
 //Gets the total from an inputed array. Used to update home/away total
 function getTotal(inputArray) {
-    let total = 0;
-    for (let i = 0; i < inputArray.length; i++) {
-        total += inputArray[i];
-    }
-    return total;
+  let total = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    total += inputArray[i];
+  }
+  return total;
+}
+
+function playChicenSong() {
+  var audio = new Audio("sounds/chickendance.mp3");
+  audio.play();
+}
+
+function playRandomSong() {
+  let randNumber = Math.floor(Math.random() * 3) + 1;
+  var audio = new Audio();
+  if (randNumber == 1) {
+    var audio = new Audio("sounds/buildup.mp3");
+  } else if (randNumber == 2) {
+    var audio = new Audio("sounds/charge1.mp3");
+  } else {
+    var audio = new Audio("sounds/charge2.mp3");
+  }
+  audio.play();
 }
