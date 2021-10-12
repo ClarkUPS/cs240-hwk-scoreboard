0eed093b44761ca38bb46a81a70368fcf0a055d3
ClarkUPS <cbrace@pugetsound.edu>
2021-10-11 17:48:11 -0700

    Began working on the functions and figureing out how to set up my value tables. Also began to work with the values in the tables

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-/**
- * Scoreboard Homework
- * @author 
- */
+//Feilds for storing data
+away = new Array() //to store values 
+home = new Array()
+awayTotal = 0
+homeTotal = 0
+
+
+//Functions:
+
+function addTo() {
+    alert(home_or_away)
+
+}
+
+function subFrom() {
+    
+
+}
+
+//Gets the total from an inputed array. Used to update home/away total
+function getTotal(inputArray) {
+    let total = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        total += inputArray[i];
+    }
+    return total;
+}
