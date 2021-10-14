502b200ec6503bfea87ad8d7defeeb897bd31779
ClarkUPS <cbrace@pugetsound.edu>
2021-10-13 17:43:10 -0700

    Developement done. Finished up polishing comments and removing unused methods and code. Project is done as best I can tell.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Feilds for storing data
 var Away = new Array(); //to store values
-var Home = new Array();
+var Home = new Array(); 
 awayTotal = 0;
 homeTotal = 0;
 
@@ -9,11 +9,12 @@ for (let i = 0; i < 8; i++) {
   Away.push(0);
   Home.push(0);
 }
-//Functions:
 
+//Functions:
+//Adds to the total and values in the table and updates total in respective rows
 function addTo() {
-  var target = Home_or_Away.value; //Home or away
-  var number = parseInt(inning.value); //The inning
+  let target = Home_or_Away.value; //Home or away
+  let number = parseInt(inning.value); //The inning
 
   if (target == "Away") {
     let tempNum = Away[number] + 1; 
@@ -26,7 +27,7 @@ function addTo() {
     awayTotal++
     templist[7].innerHTML = awayTotal;
     
-
+    //Does the same for Home
   } else {
     let tempNum = Home[number] + 1;
     Home.splice(number, 1, tempNum);
@@ -41,17 +42,47 @@ function addTo() {
   }
 }
 
-function subFrom() {} //make sure to not allow negative numbers
+//Subtracts from score board and updates total in respective rows
+function subFrom() {
+  let target = Home_or_Away.value; //Home or away
+  let number = parseInt(inning.value); //The inning
+  //Check which row to put in
+  if (target == "Away") {
+    let tempNum = Away[number]
+    //Make sure cannot go negative
+    if (tempNum > 0) {
+      let tempNum = Away[number] - 1; 
+    Away.splice(number,1,tempNum);
+    
+    let templist = document.querySelectorAll('td[id^="away"]');//groups all tags together
+    templist[number - 1].innerHTML = Away[number];
+    
+    //Sub from R
+    awayTotal--
+    templist[7].innerHTML = awayTotal;
 
-//Gets the total from an inputed array. Used to update home/away total
-function getTotal(inputArray) {
-  let total = 0;
-  for (let i = 0; i < inputArray.length; i++) {
-    total += inputArray[i];
+    }
+    //does the same for Home
+  } else {
+    let tempNum = Home[number];
+
+    if (tempNum > 0) {
+      let tempNum = Home[number] - 1;
+      Home.splice(number, 1, tempNum);
+      
+      let templist = document.querySelectorAll('td[id^="home"]')
+      templist[number - 1].innerHTML = Home[number];
+      
+      //Sub from R
+      homeTotal--
+      templist[7].innerHTML = homeTotal;
+    }
   }
-  return total;
-}
 
+
+} 
+
+//Plays the chicken song when clicked
 function playChicenSong() {
   var audio = new Audio("sounds/chickendance.mp3");
   audio.play();
@@ -59,6 +90,7 @@ function playChicenSong() {
   document.getElementById("mainImage").src = "images/sdchicken.jpg";
 }
 
+//Plays a random build up/charge song
 function playRandomSong() {
   let randNumber = Math.floor(Math.random() * 3) + 1;
   if (randNumber == 1) {
@@ -70,15 +102,5 @@ function playRandomSong() {
   }
   audio.play();
 
-  // let imageSwitch = document.querySelector(mainImage);
-  // imageSwitch.replaceWith
-  // imageSwitch.innerHTML = images / megaphone.png
-
-  //document selector
   document.getElementById("mainImage").src = "images/megaphone.png";
-  
-  document.querySelectorAll("mainImage").src = "images/megaphone.png";
-  
-
-
 }
